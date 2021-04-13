7842be2fbb4e3a14026ad5094cf86d3a53520c5b
Fellipe <fellipe_emanoel@univem.edu.br>
2021-04-12 21:05:40 -0300

    Finalizando desafio

--- src/index.js
+++ src/index.js
@@ -10,19 +10,62 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user){
+    return response.status(404).json({error: "User not found"})
+  }
+
+  request.user = user;
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request;
+
+  if((!user.pro && user.todos.length <= 10) || user.pro){
+    next();
+  }
+  return response.status(403).json({ error: "Create a todo not avaliable"})
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  const {id} = request.params;
+  const user = users.find((user) => user.username === username);
+
+  if(!user){
+    return response.status(404).json({error: "User not found"});
+  }
+
+  if(validate(id)){
+    const userTodoExist = user.todos.find((t) => t.id === id);
+    
+    if(!userTodoExist){
+      return response.status(404).json({error: "Todo not exist for this user"});
+    }
+  
+    request.user = user;
+    request.todo = userTodoExist;
+
+    next();
+  }
+  return response.status(400).json({error: "You need pass a uuid"});
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if(!user){
+    return response.status(404).json({error: "User not found"})
+  }
+
+  request.user = user;
+  next();
 }
 
 app.post('/users', (request, response) => {
